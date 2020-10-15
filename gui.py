8fc6f6a6f00668e7759bf615f7deea1418236f87
asvitaliii <a.s.vitaliii@gmail.com>
2020-10-15 20:55:00 +0300

    ver 2.0

--- gui.py
+++ gui.py
@@ -7,13 +7,14 @@ class Window(QtWidgets.QMainWindow):
     def __init__(self):
         super().__init__()
         self.__win = uic.loadUi('gui.ui')
-        self.setWindowTitle(XMLDrive().get_title())
-        self.question1.setText(XMLDrive().get_questions(0))
-        self.question1.adjustSize()
-        self.question2.setText(XMLDrive().get_questions(1))
-        self.question2.adjustSize()
-        self.question3.setText(XMLDrive().get_questions(2))
-        self.question3.adjustSize()
+        self.__win.setWindowTitle(XMLDrive().get_title())
+        self.__win.question1.setText(XMLDrive().get_questions(0))
+        self.__win.question1.adjustSize()
+        self.__win.question2.setText(XMLDrive().get_questions(1))
+        self.__win.question2.adjustSize()
+        self.__win.question3.setText(XMLDrive().get_questions(2))
+        self.__win.question3.adjustSize()
+        self.__driver = XMLDrive()
 
     def __set_slots(self):
         self.__win.ok.clicked.connect(self.finish_test)
@@ -24,10 +25,10 @@ class Window(QtWidgets.QMainWindow):
 
     def finish_test(self):
         mark = 0
-        if XMLDrive().get_answers(0) == self.__win.lineEdit.text():
+        if self.__driver.get_answers(0) == self.__win.answer1.text():
             mark += 1
-        if XMLDrive().get_answers(1) == self.__win.lineEdit_2.text():
+        if self.__driver.get_answers(1) == self.__win.answer2.text():
             mark += 1
-        if XMLDrive().get_answers(2) == self.__win.lineEdit_3.text():
+        if self.__driver.get_answers(2) == self.__win.answer3.text():
             mark += 1
         QMessageBox.information(self, 'Ваш результат:', f'{mark} бал(а).')
